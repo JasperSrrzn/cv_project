58b85207df0a4aca341ced6b829a3382bfc861c8
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-30 00:51:41 +0200

    data

--- segmentation/utils/get_weights_path.py
+++ segmentation/utils/get_weights_path.py
@@ -1,7 +1,7 @@
 from keras.utils.data_utils import get_file
 
 def get_weights_path_vgg16():
-    TF_WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
+    TF_WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/fcn_vgg16_weights_tf_dim_ordering_tf_kernels.h5'
     weights_path = get_file('fcn_vgg16_weights_tf_dim_ordering_tf_kernels.h5',TF_WEIGHTS_PATH,cache_subdir='models')
     return weights_path
 
