dbd0fc79ee37f68dea9132f82f518987a2eea81a
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-30 00:57:55 +0200

    update

--- segmentation/segmentation_model.py
+++ segmentation/segmentation_model.py
@@ -84,8 +84,8 @@ class segmentation_model(object):
 
         self.model = Model(inputs, x)
         self.model.compile(optimizer=Adam(lr=1e-3), loss=dice_coef_loss)
-        weights_path = get_weights_path_vgg16()
-        self.model.load_weights(weights_path, by_name=True)
+        weights_path = os.path.expanduser(os.path.join('~', '.keras/models/fcn_vgg16_weights_tf_dim_ordering_tf_kernels.h5'))
+        model.load_weights(weights_path, by_name=True)
         if (pretrained_weights):
             self.model.load_weights(pretrained_weights)
 
