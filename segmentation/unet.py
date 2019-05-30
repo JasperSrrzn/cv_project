9bc6b92959ab3bb29ef523c029a40a5f19f55599
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-30 23:21:36 +0200

    update

--- segmentation/unet.py
+++ segmentation/unet.py
@@ -12,43 +12,77 @@ def dice_coef(y_true, y_pred, smooth=1):
 def dice_coef_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
 
-
-
-
 class Unet(object):
     def __init__(self,n_filters,pretrained_weights=None):
         if pretrained_weights==None:
             input_size = (224, 224, 3)
             inputs = Input(input_size)
-            conv1 = conv2d_block(inputs, n_filters*1, kernel_size=3, batchnorm=True)
+
+            conv1 = Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(inputs)
+            conv1 = BatchNormalization()(conv1)
+            conv1 = Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv1)
+            conv1 = BatchNormalization()(conv1)
+            conv1 = Activation('relu')(conv1)
             pool1 = MaxPooling2D(pool_size=(2,2))(conv1)
 
-            conv2 = conv2d_block(pool1, n_filters*2, kernel_size=3, batchnorm=True)
-            pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+            conv2 = Conv2D(filters=n_filters*2, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(pool1)
+            conv2 = BatchNormalization()(conv2)
+            conv2 = Conv2D(filters=n_filters*2, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv2)
+            conv2 = BatchNormalization()(conv2)
+            conv2 = Activation('relu')(conv2)
+            pool2 = MaxPooling2D(pool_size=(2,2))(conv2)
 
-            conv3 = conv2d_block(pool2, n_filters*4, kernel_size=3, batchnorm=True)
-            pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+            conv3 = Conv2D(filters=n_filters*4, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(pool2)
+            conv3 = BatchNormalization()(conv3)
+            conv3 = Conv2D(filters=n_filters*4, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv3)
+            conv3 = BatchNormalization()(conv3)
+            conv3 = Activation('relu')(conv3)
+            pool3 = MaxPooling2D(pool_size=(2,2))(conv3)
 
-            conv4 = conv2d_block(pool3, n_filters*8, kernel_size=3, batchnorm=True)
-            pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+            conv4 = Conv2D(filters=n_filters*8, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(pool3)
+            conv4 = BatchNormalization()(conv4)
+            conv4 = Conv2D(filters=n_filters*8, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv4)
+            conv4 = BatchNormalization()(conv4)
+            conv4 = Activation('relu')(conv4)
+            pool4 = MaxPooling2D(pool_size=(2,2))(conv4)
 
-            conv5 = conv2d_block(pool4, n_filters*16, kernel_size=3, batchnorm=True)
+            conv5 = Conv2D(filters=n_filters*16, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(pool4)
+            conv5 = BatchNormalization()(conv5)
+            conv5 = Conv2D(filters=n_filters*16, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv5)
+            conv5 = BatchNormalization()(conv5)
+            conv5 = Activation('relu')(conv5)
 
             up6 = Conv2DTranspose(n_filters*8, (3,3),strides=(2,2), padding='same')(conv5)
             up6 = concatenate([up6,conv4])
-            conv6 = conv2d_block(up6, n_filters*8, kernel_size=3, batchnorm=True)
+            conv6 = Conv2D(filters=n_filters*8, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(up6)
+            conv6 = BatchNormalization()(conv6)
+            conv6 = Conv2D(filters=n_filters*8, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv6)
+            conv6 = BatchNormalization()(conv6)
+            conv6 = Activation('relu')(conv6)
 
             up7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same')(conv6)
             up7 = concatenate([up7, conv3])
-            conv7 = conv2d_block(up7, n_filters * 4, kernel_size=3, batchnorm=True)
+            conv7 = Conv2D(filters=n_filters*4, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(up7)
+            conv7 = BatchNormalization()(conv7)
+            conv7 = Conv2D(filters=n_filters*4, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv7)
+            conv7 = BatchNormalization()(conv7)
+            conv7 = Activation('relu')(conv7)
 
             up8 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2), padding='same')(conv7)
             up8 = concatenate([up8, conv2])
-            conv8 = conv2d_block(up8, n_filters * 2, kernel_size=3, batchnorm=True)
+            conv8 = Conv2D(filters=n_filters*2, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(up8)
+            conv8 = BatchNormalization()(conv8)
+            conv8 = Conv2D(filters=n_filters*2, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv8)
+            conv8 = BatchNormalization()(conv8)
+            conv8 = Activation('relu')(conv8)
 
             up9 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2), padding='same')(conv8)
             up9 = concatenate([up9, conv1])
-            conv9 = conv2d_block(up9, n_filters * 1, kernel_size=3, batchnorm=True)
+            conv9 = Conv2D(filters=n_filters*1, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(up9)
+            conv9 = BatchNormalization()(conv9)
+            conv9 = Conv2D(filters=n_filters*1, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(conv9)
+            conv9 = BatchNormalization()(conv9)
+            conv9 = Activation('relu')(conv9)
 
             outputs = Conv2D(1 , (1,1), activation='sigmoid')(conv9)
 
