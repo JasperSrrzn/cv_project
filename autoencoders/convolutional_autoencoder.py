ffa47c287cfc4046b85fcf0be35da812644f1426
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-05-31 01:27:58 +0200

    update

--- autoencoders/convolutional_autoencoder.py
+++ autoencoders/convolutional_autoencoder.py
@@ -2,6 +2,7 @@ from keras.layers import *
 from keras.models import Model, Sequential
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from keras import losses
+from keras.optimizers import *
 
 import os
 
@@ -21,33 +22,36 @@ class ConvolutionalAutoencoder(object):
 
         self.latent_dim = latent_dim
         self.n_filters = n_filters
-        if pretrained_weights == None:
+
+        if pretrained_weights==None:
             input_size = (224, 224, 3)
             inputs = Input(input_size)
-            conv1 = conv2d_block(inputs, self.n_filters * 1, kernel_size=3, batchnorm=True)
-            pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+            conv1 = conv2d_block(inputs, n_filters*1, kernel_size=3, batchnorm=True)
+            pool1 = MaxPooling2D(pool_size=(2,2))(conv1)
             pool1 = Dropout(0.2)(pool1)
 
-            conv2 = conv2d_block(pool1, self.n_filters * 2, kernel_size=3, batchnorm=True)
+            conv2 = conv2d_block(pool1, n_filters*2, kernel_size=3, batchnorm=True)
             pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
             pool2 = Dropout(0.2)(pool2)
 
-            conv3 = conv2d_block(pool2, self.n_filters * 4, kernel_size=3, batchnorm=True)
+            conv3 = conv2d_block(pool2, n_filters*4, kernel_size=3, batchnorm=True)
             pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
             pool3 = Dropout(0.2)(pool3)
 
-            conv4 = conv2d_block(pool3, self.n_filters * 8, kernel_size=3, batchnorm=True)
+            conv4 = conv2d_block(pool3, n_filters*8, kernel_size=3, batchnorm=True)
             pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
             pool4 = Dropout(0.2)(pool4)
 
-            conv5 = conv2d_block(pool4, self.n_filters * 16, kernel_size=3, batchnorm=True)
-            shape = conv5.shape
-            flatten = Flatten()(conv5)
-            flatten_shape = flatten.shape
+            conv5 = conv2d_block(pool4, n_filters*16, kernel_size=3, batchnorm=True)
 
-            f1 = Dense(flatten_shape[-1])(flatten)
+            shape = conv5.shape
+            f1 = Flatten()(conv5)
+            int_mod = Model(inputs,f1)
+            flatten_shape = int_mod.output_shape
+
+            f1 = Dense(flatten_shape[-1])(f1)
             f1 = Activation('relu')(f1)
-            f1 = BatchNormalization(f1)
+            f1 = BatchNormalization()(f1)
             latent = Dense(self.latent_dim)(f1)
 
             self.encoder = Model(input=inputs,output=latent)
@@ -55,32 +59,31 @@ class ConvolutionalAutoencoder(object):
             f2 = Dense(flatten_shape[-1])(latent)
             f2 = Activation('relu')(f2)
             f2 = BatchNormalization()(f2)
-            f2 = Dense(shape[1]*shape[2]*shape[3])(f2)
-            f2 = Reshape((shape[1],shape[2],shape[3]))(f2)
+            f2 = Dense(int(shape[1])*int(shape[2])*int(shape[3]))(f2)
+            f2 = Reshape((int(shape[1]),int(shape[2]),int(shape[3])))(f2)
 
-            up6 = Conv2DTranspose(self.n_filters * 8, (3, 3), strides=(2, 2), padding='same')(f2)
-            up6 = concatenate([up6, conv4])
+            up6 = Conv2DTranspose(n_filters*8, (3,3),strides=(2,2), padding='same')(conv5)
+            up6 = concatenate([up6,conv4])
             up6 = Dropout(0.2)(up6)
-            conv6 = conv2d_block(up6, self.n_filters * 8, kernel_size=3, batchnorm=True)
+            conv6 = conv2d_block(up6, n_filters*8, kernel_size=3, batchnorm=True)
 
-            up7 = Conv2DTranspose(self.n_filters * 4, (3, 3), strides=(2, 2), padding='same')(conv6)
+            up7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same')(conv6)
             up7 = concatenate([up7, conv3])
             up7 = Dropout(0.2)(up7)
-            conv7 = conv2d_block(up7, self.n_filters * 4, kernel_size=3, batchnorm=True)
+            conv7 = conv2d_block(up7, n_filters * 4, kernel_size=3, batchnorm=True)
 
-            up8 = Conv2DTranspose(self.n_filters * 2, (3, 3), strides=(2, 2), padding='same')(conv7)
+            up8 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2), padding='same')(conv7)
             up8 = concatenate([up8, conv2])
             up8 = Dropout(0.2)(up8)
-            conv8 = conv2d_block(up8, self.n_filters * 2, kernel_size=3, batchnorm=True)
+            conv8 = conv2d_block(up8, n_filters * 2, kernel_size=3, batchnorm=True)
 
-            up9 = Conv2DTranspose(self.n_filters * 1, (3, 3), strides=(2, 2), padding='same')(conv8)
+            up9 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2), padding='same')(conv8)
             up9 = concatenate([up9, conv1])
-            up9 = Dropout(0.2)(up9)
-            conv9 = conv2d_block(up9, self.n_filters * 1, kernel_size=3, batchnorm=True)
+            up9  = Dropout(0.2)(up9)
+            conv9 = conv2d_block(up9, n_filters * 1, kernel_size=3, batchnorm=True)
 
-            outputs = Conv2D(3, (3, 3), activation='sigmoid')(conv9)
+            outputs = Conv2D(3 , (1,1), activation='sigmoid')(conv9)
 
-            self.decoder = Model(input=latent,output=outputs)
             self.autoencoder = Model(input=inputs, output=outputs)
 
             self.autoencoder.compile(optimizer=Adam(lr=1e-3), loss='binary_crossentropy')
@@ -102,7 +105,7 @@ class ConvolutionalAutoencoder(object):
         if path is None:
             path = os.get.cwd()
         self.encoder.save_weights(os.path.join(path,prefix + "encoder_weights.h5"))
-        self.decoder.save_weights(os.path.join(path_prefix + "decoder_weights.h5"))
+
 
     def load_weights(self,path=None):
         self.autoencoder.load_weights(path)
@@ -110,8 +113,6 @@ class ConvolutionalAutoencoder(object):
     def encode(self,input):
         return self.encoder.predict(input)
 
-    def decode(self,codes):
-        return self.decoder.predict(codes)
 
     def predict(self,X):
         return self.autoencoder.predict(X)
