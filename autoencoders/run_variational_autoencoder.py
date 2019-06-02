776f75f195db4b973d501c55dc59118274473a0e
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-02 11:16:22 +0200

    update

--- autoencoders/run_variational_autoencoder.py
+++ autoencoders/run_variational_autoencoder.py
@@ -1,81 +1,59 @@
+from convolutional_autoencoder import ConvolutionalAutoencoder
 from variational_autoencoder import VariationalConvolutionalAutoencoder
 import os
 import numpy as np
-from sklearn.manifold import TSNE
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
+
+# flag for training or read from test.h5
+do_training = 1
+save_autoenco = 1
 
 #define parameters
-latent_dimension = 1000
-num_epochs = 100
+latent_dimension = 100
+num_epochs = 1000
+num_filters = 1
+loss = 'xent'
 
 #directory for stored data and stored model
 data_dir = os.path.dirname(os.getcwd())+'/output_data/'
-model_dir = os.path.dirname(os.getcwd())+'/autoencoders/saved_models/'
+model_dir = '/content/gdrive/My Drive//autoencoders/saved_models/'
 
 #name for storing best_autoencoder
-name = 'variationalautoencoder_xent_'+str(latent_dimension)+'.h5'
-
-# read train data
-X = np.load(data_dir+'X_train.npy')
-Y = np.load(data_dir+'Y_train.npy')
-
-#split training data in train and validation set
-X_train, X_validation, Y_train, Y_validation = train_test_split(X,Y,test_size=0.2,random_state=42)
-
-Y_train = np.argmax(Y_train,axis=1)+1
-Y_validation = np.argmax(Y_validation,axis=1)+1
-
-#read test data
-X_test = np.load(data_dir+'X_test.npy')
-Y_test = np.load(data_dir+'Y_test.npy')
-Y_test = np.argmax(Y_test,axis=1)+1
+name = 'variational_autoencoder_'+loss+'_'+str(latent_dimension)+'.h5'
 
 
-vae = VariationalConvolutionalAutoencoder(latent_dimension)
-vae.fit(X_train,X_validation,name,epochs=num_epochs)
-
-best_vae = VariationalConvolutionalAutoencoder(latent_dimension)
-best_vae.load_weights(model_dir+name)
-
-X_train_enc = best_vae.encode(X_train)
-X_test_enc = best_vae.encode(X_test)
-X_test_rec = best_vae.decode(X_test_enc)
-X_train_rec = best_vae.decode(X_train_enc)
-print(mean_squared_error(X_test.flatten(),X_test_rec.flatten()))
-
-#make tSNE plots to visualize the distribution of data points in 2d
-train_codes_embedded = TSNE(n_components=2).fit_transform(X_train_enc)
-test_codes_embedded = TSNE(n_components=2).fit_transform(X_test_enc)
-c_dict = {1: 'red', 2:'blue',3:'green'}
-
-fig, ax = plt.subplots()
-legend_labels = []
-for g in range(len(train_codes_embedded)):
-    if Y_train[g] not in legend_labels:
-        plt.scatter(train_codes_embedded[g,0], train_codes_embedded[g,1], c=c_dict[Y_train[g]], label=Y_train[g])
-        legend_labels += [Y_train[g]]
-    else:
-        plt.scatter(train_codes_embedded[g, 0], train_codes_embedded[g, 1], c=c_dict[Y_train[g]])
-plt.legend()
-plt.title('tSNE on training set')
-try:
-    os.mkdir('./figures/'+name[:-3])
-except:
-    pass
-plt.savefig('./figures/'+name[:-3]+'/tsne_train.eps')
+# read train,validation and test data
+X_train = np.load(data_dir+'x_train_img.npy')
+X_validation = np.load(data_dir+'x_val_img.npy')
+X_test = np.load(data_dir+'x_test_img.npy')
 
 
-fig, ax = plt.subplots()
-legend_labels = []
-for g in range(len(test_codes_embedded)):
-    if Y_test[g] not in legend_labels:
-        plt.scatter(test_codes_embedded[g,0], test_codes_embedded[g,1], c=c_dict[Y_test[g]], label=Y_test[g])
-        legend_labels += [Y_test[g]]
-    else:
-        plt.scatter(test_codes_embedded[g, 0], test_codes_embedded[g, 1], c=c_dict[Y_test[g]])
-plt.legend()
-plt.title('tSNE on test set')
-plt.savefig('./figures/'+name[:-3]+'/tsne_test.eps')
 
+if do_training == 1:
+    print('starting training...')
+    #initialize model
+    autoencoder = VariationalConvolutionalAutoencoder(latent_dimension,num_filters)
+    #train model
+    autoencoder.fit(X_train=X_train,X_validation=X_validation,name=name,epochs=num_epochs)
+
+#select the best model (stored)
+best_autoencoder = VariationalConvolutionalAutoencoder(latent_dimension,num_filters)
+best_autoencoder.load_weights(model_dir+name)
+
+
+#encode the training set and test set
+X_train_enc = best_autoencoder.encode(X_train)
+X_test_enc = best_autoencoder.encode(X_test)
+X_val_enc = best_autoencoder.encode(X_validation)
+
+#reconstruct
+X_test_rec = best_autoencoder.predict(X_test)
+
+np.save('/content/gdrive/My Drive/autoencoders/reconstructions/X_pred_rec_'+name[:-3]+'.npy',X_test_rec)
+
+
+# save encoded vars
+if save_autoenco == 1:
+    print('saving encoded vars')
+    np.save('/content/gdrive/My Drive/autoencoders/encoded/X_train_enc_'+name[:-3]+'.npy',X_train_enc)
+    np.save('/content/gdrive/My Drive/autoencoders/encoded/X_test_enc_'+name[:-3]+'.npy',X_test_enc)
+    np.save('/content/gdrive/My Drive/autoencoders/encoded/X_val_enc_'+name[:-3]+'.npy',X_val_enc)
--- autoencoders/variational_autoencoder.py
+++ autoencoders/variational_autoencoder.py
@@ -12,111 +12,119 @@ def sampling(args):
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5*z_log_var)*epsilon
 
+def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
+    x =Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same',kernel_regularizer=regularizers.l2(0.01))(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    """
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same',kernel_regularizer=regularizers.l2(0.001))(x)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    """
+    return x
+
 class VariationalConvolutionalAutoencoder(object):
 
-    def __init__(self,latent_dim):
+    def __init__(self,latent_dim,n_filters,pretrained_weights=None):
+
         self.latent_dim = latent_dim
+        self.n_filters = n_filters
 
-        # encoder architecture
-        self.encoder = Sequential()
-        self.encoder.add(InputLayer((224, 224, 3)))
-        self.encoder.add(Conv2D(4, (3, 3), padding='same'))
-        self.encoder.add(BatchNormalization())
-        self.encoder.add(Activation('relu'))
-        self.encoder.add(MaxPooling2D((2, 2), padding='same'))
-        self.encoder.add(Conv2D(8, (3, 3), padding='same'))
-        self.encoder.add(BatchNormalization())
-        self.encoder.add(Activation('relu'))
-        self.encoder.add(MaxPooling2D((2, 2), padding='same'))
-        self.encoder.add(Conv2D(16, (3, 3), padding='same'))
-        self.encoder.add(BatchNormalization())
-        self.encoder.add(Activation('relu'))
-        self.encoder.add(MaxPooling2D((2, 2), padding='same'))
-        self.encoder.add(Conv2D(32, (3, 3), padding='same'))
-        self.encoder.add(BatchNormalization())
-        self.encoder.add(Activation('relu'))
-        self.encoder.add(MaxPooling2D((2, 2), padding='same'))
-        self.encoder.add(Conv2D(64, (3, 3), padding='same'))
-        self.encoder.add(BatchNormalization())
-        self.encoder.add(Activation('relu'))
-        self.encoder.add(MaxPooling2D((2, 2), padding='same'))
-        shape = self.encoder.output_shape
-        self.encoder.add(Flatten())
-        flattened_shape = self.encoder.output_shape
+        if pretrained_weights==None:
+            input_size = (224, 224, 3)
+            inputs = Input(input_size)
+            conv1 = conv2d_block(inputs, n_filters*1, kernel_size=3, batchnorm=True)
+            pool1 = MaxPooling2D(pool_size=(2,2))(conv1)
+            pool1 = Dropout(0.2)(pool1)
 
-        #generate latent representation
-        self.mean_encoder = Sequential()
-        self.mean_encoder.add(Dense(flattened_shape[-1]))
-        self.mean_encoder.add(Activation('relu'))
-        self.mean_encoder.add(BatchNormalization())
-        self.mean_encoder.add(Dense(self.latent_dim))
+            conv2 = conv2d_block(pool1, n_filters*2, kernel_size=3, batchnorm=True)
+            pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+            pool2 = Dropout(0.2)(pool2)
 
+            conv3 = conv2d_block(pool2, n_filters*4, kernel_size=3, batchnorm=True)
+            pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+            pool3 = Dropout(0.2)(pool3)
 
-        self.logvar_encoder = Sequential()
-        self.logvar_encoder.add(Dense(flattened_shape[-1]))
-        self.logvar_encoder.add(Activation('relu'))
-        self.logvar_encoder.add(BatchNormalization())
-        self.logvar_encoder.add(Dense(self.latent_dim))
-        self.logvar_encoder.add(Dense(self.latent_dim))
+            conv4 = conv2d_block(pool3, n_filters*8, kernel_size=3, batchnorm=True)
+            pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+            pool4 = Dropout(0.2)(pool4)
+
+            convx = conv2d_block(pool4, n_filters*16, kernel_size=3, batchnorm=True)
+            poolx = MaxPooling2D(pool_size=(2, 2))(convx)
+            poolx = Dropout(0.2)(poolx)
+
+            conv5 = conv2d_block(poolx, n_filters*32, kernel_size=3, batchnorm=True)
+
+            shape = conv5.shape
+            latent = Flatten()(conv5)
+
+            #generate latent representation
+            self.mean_encoder = Sequential()
+            self.mean_encoder.add(BatchNormalization())
+            self.mean_encoder.add(Dense(self.latent_dim))
 
 
-        input = Input((224, 224, 3))
-        code = self.encoder(input)
-        z_mean = self.mean_encoder(code)
-        z_logvar = self.logvar_encoder(code)
-        sample_layer = Lambda(sampling)
-        sample = sample_layer([z_mean, z_logvar])
-
-        self.latent_encoder = Model(input,z_mean)
-
-        self.decoder = Sequential()
-        self.decoder.add(Dense(100))
-        self.decoder.add(Activation('relu'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Dense(shape[1]*shape[2]*shape[3]))
-        self.decoder.add(Reshape((shape[1],shape[2],shape[3])))
-        self.decoder.add(Conv2DTranspose(64, (3, 3), padding='same'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Activation('relu'))
-        self.decoder.add(UpSampling2D((2, 2)))
-        self.decoder.add(Conv2DTranspose(32, (3, 3), padding='same'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Activation('relu'))
-        self.decoder.add(UpSampling2D((2, 2)))
-        self.decoder.add(Conv2DTranspose(16, (3, 3), padding='same'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Activation('relu'))
-        self.decoder.add(UpSampling2D((2, 2)))
-        self.decoder.add(Conv2DTranspose(8, (3, 3), padding='same'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Activation('relu'))
-        self.decoder.add(UpSampling2D((2, 2)))
-        self.decoder.add(Conv2DTranspose(4, (3, 3), padding='same'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Activation('relu'))
-        self.decoder.add(UpSampling2D((2, 2)))
-        self.decoder.add(Conv2DTranspose(3, (3, 3), padding='same'))
-        self.decoder.add(BatchNormalization())
-        self.decoder.add(Activation('sigmoid'))
+            self.logvar_encoder = Sequential()
+            self.logvar_encoder.add(BatchNormalization())
+            self.logvar_encoder.add(Dense(self.latent_dim))
 
 
-        reconstructed = self.decoder(sample)
+            input = Input((224, 224, 3))
+            code = self.encoder(input)
+            z_mean = self.mean_encoder(code)
+            z_logvar = self.logvar_encoder(code)
+            sample_layer = Lambda(sampling)
+            sample = sample_layer([z_mean, z_logvar])
 
-        self.vae = Model(input,reconstructed)
-        reconstruction_loss =  binary_crossentropy(K.flatten(input), K.flatten(reconstructed))
-        kl_loss = 1 + z_logvar - K.square(z_mean) - K.exp(z_logvar)
-        kl_loss = K.sum(kl_loss, axis=-1)
-        kl_loss *= -0.5
-        vae_loss = K.mean(reconstruction_loss + kl_loss)
-        self.vae.add_loss(vae_loss)
-        self.vae.compile(optimizer='adam')
+            self.latent_encoder = Model(input,z_mean)
+
+            f2 = Reshape((int(shape[1]),int(shape[2]),int(shape[3])))(sample)
+
+            upy = Conv2DTranspose(n_filters*8, (3,3),strides=(2,2), padding='same')(f2)
+            upy = concatenate([upy,convx])
+            upy = Dropout(0.2)(upy)
+            convy = conv2d_block(upy, n_filters*8, kernel_size=3, batchnorm=True)
+
+            up6 = Conv2DTranspose(n_filters*8, (3,3),strides=(2,2), padding='same')(convy)
+            up6 = concatenate([up6,conv4])
+            up6 = Dropout(0.2)(up6)
+            conv6 = conv2d_block(up6, n_filters*8, kernel_size=3, batchnorm=True)
+
+            up7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same')(conv6)
+            up7 = concatenate([up7, conv3])
+            up7 = Dropout(0.2)(up7)
+            conv7 = conv2d_block(up7, n_filters * 4, kernel_size=3, batchnorm=True)
+
+            up8 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2), padding='same')(conv7)
+            up8 = concatenate([up8, conv2])
+            up8 = Dropout(0.2)(up8)
+            conv8 = conv2d_block(up8, n_filters * 2, kernel_size=3, batchnorm=True)
+
+            up9 = Conv2DTranspose(n_filters * 1, (3, 3), strides=(2, 2), padding='same')(conv8)
+            up9 = concatenate([up9, conv1])
+            up9  = Dropout(0.2)(up9)
+            conv9 = conv2d_block(up9, n_filters * 1, kernel_size=3, batchnorm=True)
+
+            outputs = Conv2D(3 , (1,1), activation='sigmoid')(conv9)
+
+
+            self.vae = Model(input,outputs)
+            reconstruction_loss =  binary_crossentropy(K.flatten(input), K.flatten(outputs))
+            kl_loss = 1 + z_logvar - K.square(z_mean) - K.exp(z_logvar)
+            kl_loss = K.sum(kl_loss, axis=-1)
+            kl_loss *= -0.5
+            vae_loss = K.mean(reconstruction_loss + kl_loss)
+            self.vae.add_loss(vae_loss)
+            self.vae.compile(optimizer='adam')
 
     def fit(self,X_train,X_validation,name,epochs=50):
-        model_dir = os.path.dirname(os.getcwd()) + '/autoencoders/saved_models/'
+        model_dir = '/content/gdrive/My Drive/autoencoders/saved_models/'
         callbacks = [ModelCheckpoint(model_dir + name, monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=False),
                      EarlyStopping(patience=20, verbose=1),
                      ReduceLROnPlateau(patience=10, verbose=1),
-                     TensorBoard(log_dir='./logs/'+name[:-3], histogram_freq=0, batch_size=32, write_graph=True,
+                     TensorBoard(log_dir='/content/gdrive/My Drive/autoencoders/logs/'+name[:-3], histogram_freq=0, batch_size=32, write_graph=True,
                                  write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None,
                                  embeddings_metadata=None, embeddings_data=None, update_freq='epoch')]
         self.vae.fit(x=X_train,epochs=epochs,batch_size=2,validation_data=(X_validation,None),callbacks=callbacks)
@@ -130,7 +138,8 @@ class VariationalConvolutionalAutoencoder(object):
     def load_weights(self,path=None):
         self.vae.load_weights(path)
 
+    def predict(self,X):
+        return self.vae.apredict(X)
 
-
-
-
+    def get_weights(self):
+        return self.vae.get_weights()
