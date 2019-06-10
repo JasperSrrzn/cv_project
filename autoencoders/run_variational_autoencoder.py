030456eef2abf9739f353637976f1c413d06fa2d
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-10 12:28:48 +0200

    upate

--- autoencoders/run_variational_autoencoder.py
+++ autoencoders/run_variational_autoencoder.py
@@ -1,16 +1,13 @@
-from convolutional_autoencoder import ConvolutionalAutoencoder
 from variational_autoencoder import VariationalConvolutionalAutoencoder
 import os
 import numpy as np
-from keras.layers import *
-from keras.preprocessing.image import ImageDataGenerator
 
 # flag for training or read from test.h5
 do_training = 1
 save_autoenco = 1
 
 #define parameters
-latent_dimension = 10
+latent_dimension = 1000
 num_epochs = 1000
 num_filters = 8
 loss = 'xent'
@@ -20,13 +17,15 @@ data_dir = os.path.dirname(os.getcwd())+'/output_data/'
 model_dir = '/content/gdrive/My Drive//autoencoders/saved_models/'
 
 #name for storing best_autoencoder
-name = 'variational_autoencoder_'+loss+'_'+str(latent_dimension)+'.h5'
+name = 'autoencoder_'+loss+'_'+str(latent_dimension)+'.h5'
 
 
 # read train,validation and test data
 X_train = np.load(data_dir+'x_train_img.npy')
 X_validation = np.load(data_dir+'x_val_img.npy')
 X_test = np.load(data_dir+'x_test_img.npy')
+
+
 
 if do_training == 1:
     print('starting training...')
