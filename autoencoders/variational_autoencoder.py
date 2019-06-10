d6927d7ac90f4f846d776798387a98d86248e0c1
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-10 13:21:07 +0200

    upate

--- autoencoders/variational_autoencoder.py
+++ autoencoders/variational_autoencoder.py
@@ -151,7 +151,8 @@ class VariationalConvolutionalAutoencoder(object):
             kl_loss *= -0.5
             vae_loss = K.mean(reconstruction_loss + kl_loss)
             self.vae.add_loss(vae_loss)
-            self.vae.compile(optimizer='adam')
+            adam = Adam(lr=1e-4)
+            self.vae.compile(optimizer=adam)
 
     def fit(self,X_train,X_validation,name,epochs=50):
         model_dir = '/content/gdrive/My Drive/autoencoders/saved_models/'
