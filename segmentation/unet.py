d8fad4665d2a041ae2ee142fb19b784d058c80a7
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-12 23:45:54 +0200

    update

--- segmentation/unet.py
+++ segmentation/unet.py
@@ -64,18 +64,16 @@ def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
     """
     constructs a block of convolutional layers and batchnormalization layers
     """
-    x =Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',
-              padding='same')(input_tensor)
+    x =Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),
+              kernel_initializer='he_normal',padding='same')(input_tensor)
     if batchnorm:
         x = BatchNormalization()(x)
     x = Activation('relu')(x)
-
-    x = Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',
-              padding='same')(x)
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),
+               kernel_initializer='he_normal',padding='same')(x)
     if batchnorm:
         x = BatchNormalization()(x)
     x = Activation('relu')(x)
-
     return x
 
 
