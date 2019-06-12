1badbb7c0db30ec1d0e692014cbf37ae2de9b332
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-12 23:28:17 +0200

    update

--- segmentation/unet.py
+++ segmentation/unet.py
@@ -6,100 +6,64 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, Ear
 from keras import backend as K
 import tensorflow as tf
 
+
+class TrainValTensorBoard(TensorBoard):
+    """
+    tensorboard to plot both validation and training curves on one figure
+    """
+    def __init__(self, log_dir='./logs', **kwargs):
+        # Make the original `TensorBoard` log to a subdirectory 'training'
+        training_log_dir = os.path.join(log_dir, 'training')
+        super(TrainValTensorBoard, self).__init__(training_log_dir, **kwargs)
+
+        # Log the validation metrics to a separate subdirectory
+        self.val_log_dir = os.path.join(log_dir, 'validation')
+
+    def set_model(self, model):
+        # Setup writer for validation metrics
+        self.val_writer = tf.summary.FileWriter(self.val_log_dir)
+        super(TrainValTensorBoard, self).set_model(model)
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Pop the validation logs and handle them separately with
+        # `self.val_writer`. Also rename the keys so that they can
+        # be plotted on the same figure with the training metrics
+        logs = logs or {}
+        val_logs = {k.replace('val_', ''): v for k, v in logs.items() if k.startswith('val_')}
+        for name, value in val_logs.items():
+            summary = tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.val_writer.add_summary(summary, epoch)
+        self.val_writer.flush()
+
+        # Pass the remaining logs to `TensorBoard.on_epoch_end`
+        logs = {k: v for k, v in logs.items() if not k.startswith('val_')}
+        super(TrainValTensorBoard, self).on_epoch_end(epoch, logs)
+
+    def on_train_end(self, logs=None):
+        super(TrainValTensorBoard, self).on_train_end(logs)
+        self.val_writer.close()
+
 def dice_coef(y_true, y_pred, smooth=1):
+    """
+    calculates the dice coefficient
+    """
     intersection = K.sum(K.abs(y_true * y_pred))
     return (2. * intersection + smooth) / (K.sum(K.square(y_true),) + K.sum(K.square(y_pred)) + smooth)
 
 def dice_coef_loss(y_true, y_pred):
+    """
+    calculates the dice loss
+    """
     return 1-dice_coef(y_true, y_pred)
-
-def lovasz_grad(gt_sorted):
-    """
-    Computes gradient of the Lovasz extension w.r.t sorted errors
-    See Alg. 1 in paper
-    """
-    gts = tf.reduce_sum(gt_sorted)
-    intersection = gts - tf.cumsum(gt_sorted)
-    union = gts + tf.cumsum(1. - gt_sorted)
-    jaccard = 1. - intersection / union
-    jaccard = tf.concat((jaccard[0:1], jaccard[1:] - jaccard[:-1]), 0)
-    return jaccard
-
-
-# --------------------------- BINARY LOSSES ---------------------------
-
-def lovasz_hinge(logits, labels, per_image=True, ignore=None):
-    """
-    Binary Lovasz hinge loss
-      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
-      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
-      per_image: compute the loss per image instead of per batch
-      ignore: void class id
-    """
-    if per_image:
-        def treat_image(log_lab):
-            log, lab = log_lab
-            log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
-            log, lab = flatten_binary_scores(log, lab, ignore)
-            return lovasz_hinge_flat(log, lab)
-        losses = tf.map_fn(treat_image, (logits, labels), dtype=tf.float32)
-        loss = tf.reduce_mean(losses)
-    else:
-        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
-    return loss
-
-
-def lovasz_hinge_flat(logits, labels):
-    """
-    Binary Lovasz hinge loss
-      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
-      labels: [P] Tensor, binary ground truth labels (0 or 1)
-      ignore: label to ignore
-    """
-
-    def compute_loss():
-        labelsf = tf.cast(labels, logits.dtype)
-        signs = 2. * labelsf - 1.
-        errors = 1. - logits * tf.stop_gradient(signs)
-        errors_sorted, perm = tf.nn.top_k(errors, k=tf.shape(errors)[0], name="descending_sort")
-        gt_sorted = tf.gather(labelsf, perm)
-        grad = lovasz_grad(gt_sorted)
-        loss = tf.tensordot(tf.nn.relu(errors_sorted), tf.stop_gradient(grad), 1, name="loss_non_void")
-        return loss
-
-    # deal with the void prediction case (only void pixels)
-    loss = tf.cond(tf.equal(tf.shape(logits)[0], 0),
-                   lambda: tf.reduce_sum(logits) * 0.,
-                   compute_loss,
-                   strict=True,
-                   name="loss"
-                   )
-    return loss
-
-
-def flatten_binary_scores(scores, labels, ignore=None):
-    """
-    Flattens predictions in the batch (binary case)
-    Remove labels equal to 'ignore'
-    """
-    scores = tf.reshape(scores, (-1,))
-    labels = tf.reshape(labels, (-1,))
-    if ignore is None:
-        return scores, labels
-    valid = tf.not_equal(labels, ignore)
-    vscores = tf.boolean_mask(scores, valid, name='valid_scores')
-    vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
-    return vscores, vlabels
-
-def lovasz_loss(y_true, y_pred):
-    y_true, y_pred = K.cast(K.squeeze(y_true, -1), 'int32'), K.cast(K.squeeze(y_pred, -1), 'float32')
-    #logits = K.log(y_pred / (1. - y_pred))
-    logits = y_pred #Jiaxin
-    loss = lovasz_hinge(logits, y_true, per_image = True, ignore = None)
-    return loss
 
 
 def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
+    """
+    constructs a block of convolutional layers and batchnormalization layers
+    """
     x =Conv2D(filters=n_filters, kernel_size=(kernel_size,kernel_size),kernel_initializer='he_normal',padding='same')(input_tensor)
     if batchnorm:
         x = BatchNormalization()(x)
@@ -112,7 +76,13 @@ def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
 
 
 class Unet(object):
+    """
+    constructs an object to easily call a U-net
+    """
     def __init__(self,n_filters,pretrained_weights=None):
+        """
+        initializes the architecture of the model
+        """
         if pretrained_weights==None:
             input_size = (224, 224, 3)
             inputs = Input(input_size)
@@ -164,32 +134,32 @@ class Unet(object):
                 self.model.load_weights(pretrained_weights)
 
     def fit(self,X_train,y_train,X_validation,y_validation,name,epochs=50):
+        """
+        function to fit the model constructed in the def __init__ function.
+        """
         model_dir = '/content/gdrive/My Drive/segmentation/saved_models/'
-        callbacks = [ModelCheckpoint(model_dir + name, monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=False),
-                     EarlyStopping(patience=20, verbose=1),
-                     ReduceLROnPlateau(patience=10, verbose=1),
-                     TensorBoard(log_dir='./logs/' + name[:-3], histogram_freq=0, batch_size=32, write_graph=True,
-                                 write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None,
-                                 embeddings_metadata=None, embeddings_data=None, update_freq='epoch')]
-        self.model.fit(x=X_train, y=y_train, epochs=epochs, validation_data=[X_validation, y_validation],
-                       callbacks=callbacks)
+        callbacks = [ModelCheckpoint(model_dir+name,monitor='val_loss', verbose=1, save_best_only=True,
+                                    save_weights_only=False),
+                    EarlyStopping(patience=20, verbose=1),
+                    ReduceLROnPlateau(patience=10, verbose=1),
+                    TrainValTensorBoard(log_dir='/content/gdrive/My Drive/segmentation/logs/'+name[:-3],
+                                histogram_freq=0, batch_size=32, write_graph=True, write_grads=False,
+                                write_images=False, embeddings_freq=0, embeddings_layer_names=None,
+                                embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+                                ]
+        self.model.fit(x=X_train, y=y_train, epochs=epochs,
+                       validation_data=[X_validation, y_validation],callbacks=callbacks)
 
-    def fit_generator(self,X_train,y_train,X_validation,y_validation,datagen,name,epochs=50):
-        model_dir = os.path.dirname(os.getcwd()) + '/segmentation/saved_models/'
-        callbacks = [ModelCheckpoint(model_dir + name, monitor='val_loss', verbose=1, save_best_only=True,
-                                     save_weights_only=False),
-                     EarlyStopping(patience=20, verbose=1),
-                     ReduceLROnPlateau(patience=10, verbose=1),
-                     TensorBoard(log_dir='./logs/' + name[:-3], histogram_freq=0, batch_size=32, write_graph=True,
-                                 write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None,
-                                 embeddings_metadata=None, embeddings_data=None, update_freq='epoch')]
-        datagen.fit(X_train)
-        self.model.fit_generator(datagen.flow(x=X_train, y=y_train, batch_size=16), steps_per_epoch=100, epochs=epochs,validation_data=[X_validation, y_validation],
-                       callbacks=callbacks)
 
     def predict(self,X):
+        """
+        function to reconstruct input images
+        """
         return self.model.predict(X)
 
 
     def load_weights(self,path=None):
+        """
+        function to load weight parameters in the model architecture
+        """
         self.model.load_weights(path)
