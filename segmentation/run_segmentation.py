d85bd687794f8552f56a45d760e7e826d31981e0
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-12 22:50:42 +0200

    update

--- segmentation/run_segmentation.py
+++ segmentation/run_segmentation.py
@@ -13,7 +13,7 @@ data_dir = os.path.dirname(os.getcwd())+'/output_data/'
 model_dir = '/content/gdrive/My Drive/segmentation/saved_models/'
 
 #name
-name = 'unet_dice.h5'
+name = 'unet_lovasz.h5'
 # read train data
 X_train = np.load(data_dir+'x_train_img.npy')
 Y_train = np.load(data_dir+'y_train_seg.npy')
@@ -38,4 +38,4 @@ best_model.load_weights(model_dir+name)
 predictions_test = best_model.predict(X_test)
 predictions_train = best_model.predict(X_train)
 
-np.save('/content/gdrive/My Drive/segmentation/test_segmentations_dice.npy',predictions_test)
+np.save('/content/gdrive/My Drive/segmentation/test_segmentations_lovasz.npy',predictions_test)
