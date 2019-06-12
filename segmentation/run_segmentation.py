467ee347f5f72a7ce8171bfdd04f107d5836ecad
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-12 19:45:45 +0200

    update

--- segmentation/run_segmentation.py
+++ segmentation/run_segmentation.py
@@ -8,7 +8,7 @@ from unet import Unet
 from keras.preprocessing.image import ImageDataGenerator
 
 n_classes = 2
-num_epochs = 100
+num_epochs = 1000
 data_dir = os.path.dirname(os.getcwd())+'/output_data/'
 model_dir = os.path.dirname(os.getcwd())+'/segmentation/saved_models/'
 
