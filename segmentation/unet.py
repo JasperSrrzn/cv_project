b6d383d3c35c4dd4c3096fa5f2b90ef441c0a6b4
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-12 23:28:52 +0200

    update

--- segmentation/unet.py
+++ segmentation/unet.py
@@ -128,7 +128,7 @@ class Unet(object):
 
             self.model = Model(input=inputs, output=outputs)
 
-            self.model.compile(optimizer=Adam(lr=1e-3), loss=lovasz_loss)
+            self.model.compile(optimizer=Adam(lr=1e-3), loss=dice_coef_loss)
 
             if (pretrained_weights):
                 self.model.load_weights(pretrained_weights)
