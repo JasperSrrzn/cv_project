89a1654550c04e68a99ed1f6ee02422180e4d725
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2019-06-14 10:39:43 +0200

    update

--- autoencoders/variational_autoencoder.py
+++ autoencoders/variational_autoencoder.py
@@ -157,7 +157,7 @@ class VariationalConvolutionalAutoencoder(object):
             kl_loss *= -0.5
             vae_loss = K.mean(reconstruction_loss + kl_loss)
             self.vae.add_loss(vae_loss)
-            adam = Adam(lr=1e-4)
+            adam = Adam(lr=1e-3)
             self.vae.compile(optimizer=adam)
 
     def fit(self,X_train,X_validation,name,epochs=50):
